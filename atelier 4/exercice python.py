63554957fa77624e3442b83c0abebc3ec419db63
Nait Abderrahmane Hamza <naitabderrahmane.hamza@ensam-casa.ma>
2025-11-01 22:45:14 +0100

    Update exercice python.py

--- atelier 4/exercice python.py
+++ atelier 4/exercice python.py
@@ -1,11 +1,6 @@
-f= open("table_de_multiplication.txt","w+")
-c=1
-while c!= 10:
-    f.write("table de multiplication de"+" "+str(c)+" "+"est"+"\n")
-    for i in range(0,11):
-       
-        f.write(str(c)+"*"+str(i)+"="+str(c*i)+"\n")
-    c+=1
-
-    
-
+with open("table_de_multiplication.txt", "w") as f:
+    for c in range(1, 11):
+        f.write(f"Table de multiplication de {c} :\n")
+        for i in range(0, 11):
+            f.write(f"{c} * {i} = {c*i}\n")
+        f.write("\n")
